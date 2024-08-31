65fa9c3271625e76c13a0264644ee91024d51d46
aman <13aman13ag@gmail.com>
2024-08-31 13:49:56 +0530

    implemented adapter pattern

---
+++ src/main/java/com/example/design/lldPatterns/adapterPattern/pattern2/ThreePinSocket.java
@@ -0,0 +1,5 @@
+package com.example.design.lldPatterns.adapterPattern.pattern2;
+
+interface ThreePinSocket {
+    void connectThreePin();
+}
---
+++ src/main/java/com/example/design/lldPatterns/adapterPattern/pattern2/TwoPinPlug.java
@@ -0,0 +1,7 @@
+package com.example.design.lldPatterns.adapterPattern.pattern2;
+
+class TwoPinPlug {
+    void connectTwoPin() {
+        System.out.println("Two-pin plug connected.");
+    }
+}
